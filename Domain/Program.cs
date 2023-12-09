7911a1ff7725c6dccc9f7f3592cce44c19568f75
GDIMOKS <57040046+GDIMOKS@users.noreply.github.com>
2023-12-09 23:58:33 +0300

    add metrics

--- Domain/Program.cs
+++ Domain/Program.cs
@@ -1,4 +1,7 @@
+using App.Metrics.AspNetCore;
+using App.Metrics.Formatters.Prometheus;
 using DataAccess;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using Services.Interfaces;
 using Services.Realization;
@@ -8,11 +11,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<SongsDbContext>(x => x.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
 
 builder.Services.AddScoped<ISongService, SongService>();
 
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+builder.Services.AddMetrics();
+builder.Services.AddMetricsTrackingMiddleware();
+
+builder.Host
+    .UseMetricsWebTracking()
+    .UseMetrics(options => { options.EndpointOptions = endpointsOptions =>
+    {
+        endpointsOptions.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+        endpointsOptions.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
+        endpointsOptions.EnvironmentInfoEndpointEnabled = false;
+    };
+});
 
 var app = builder.Build();
 
@@ -21,6 +39,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+/*
+app.UseEnvInfoEndpoint();
+*/
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
